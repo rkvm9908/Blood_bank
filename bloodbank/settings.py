e8e834e5d107f7954d7cad376b07827725c407bf
Mathuprasanth.R.K <148639654+rkvm9908@users.noreply.github.com>
2026-01-01 04:47:12 +0530

    Update settings.py

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -118,7 +118,7 @@ USE_I18N = True
 USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.sendgrid.net')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
