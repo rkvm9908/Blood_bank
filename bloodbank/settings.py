255f0af7e2dbe7208ec868324587ab9419bc98e6
Mathuprasanth.R.K <148639654+rkvm9908@users.noreply.github.com>
2026-01-01 02:50:47 +0530

    Update settings.py

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -119,8 +119,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'srms1161@gmail.com'
-EMAIL_HOST_PASSWORD = 'hvgmnxvmbqkuqhda'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 # Static files (CSS, JavaScript, Images)
